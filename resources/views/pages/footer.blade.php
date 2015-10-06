1db985314817b01ca18ceacab7212fa57e87d0a6
Brent Manza <brent.manza@gmail.com>
2015-10-06 10:28:46 -0400

    initial setup

---
+++ resources/views/pages/footer.blade.php
@@ -0,0 +1,7 @@
+
+<footer>
+	<p> &copy; Brent Manza 2015 </p>
+</footer>
+
+	</body> 
+</html>
---
+++ resources/views/pages/header.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Movie App</title>
+
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+    <link href="carousel.css" rel="stylesheet">
+  </head>
