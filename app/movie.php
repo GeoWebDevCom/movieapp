cc619068c4e3a4aa4c55aa4cbf89e57b2ae53a64
Brent Manza <brent.manza@gmail.com>
2015-10-06 11:44:56 -0400

    tmdb api

--- app/movie.php
+++ app/movie.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class movie extends Model
 {
     public static function now_playing(){
-    	$movies = file_get_contents(""); //tmdb request here
+    	$movies = file_get_contents("http://api.themoviedb.org/3/movie/now_playing?api_key=ed85f5d2d688e1a2deb331c752f3dd14");
     	$movies = html_entity_decode($movies);
     	$movies = json_decode($movies, true);
 
