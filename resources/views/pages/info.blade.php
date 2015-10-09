6d904ac9ec9b910412517a6d22df88c6291b676f
Brent Manza <brent.manza@gmail.com>
2015-10-08 23:13:58 -0400

    minor tweaks

--- resources/views/pages/info.blade.php
+++ resources/views/pages/info.blade.php
@@ -4,7 +4,11 @@
 
 <img class="first-slide" id = "backdropimg" src="http://image.tmdb.org/t/p/original/{{$info['backdrop_path']}}" alt="Second slide" >
 
-<h3>{{$info['vote_average']}} / 10 </h3>
+<h3>{{$info['vote_average']}} / 10 </h3> 
+
+<h3> {{$info['vote_count']}} votes </h3>
+
+<h3>{{$info['runtime']}} minutes</h3>
 
 <p>{{$info['overview']}}</p>
 
